69ab5ee8db02b6de50849f0d505075140ef03f22
zhadan93 <zhadan.evgenya@yandex.ru>
2021-04-01 13:04:58 +0300

    refactor: add meaningful alternative text for images according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="btn delete-btn";
   deleteButtonImg.src='./remove.svg';
   deleteButtonImg.className="delete-btn-img"
+  deleteButtonImg.alt="delete";
   deleteButton.appendChild(deleteButtonImg);
 
 
